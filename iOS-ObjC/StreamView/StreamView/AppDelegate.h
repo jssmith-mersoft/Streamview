4fad42a0be88a34c7a1b143f846576afe7f165c3
Jeff Smith <jssmith@mersoft.com>
2018-10-25 10:38:20 -0500

    missined a URL update

--- iOS-ObjC/StreamView/StreamView/AppDelegate.h
+++ iOS-ObjC/StreamView/StreamView/AppDelegate.h
@@ -15,8 +15,8 @@
 //#define kMoveURLPepper @"wss://dev.move.pepperos.io/ws"
 #define kMoveURLPepper @"wss://stage.move.pepperos.io/ws"
 //#define kMoveURLPepper @"wss://prod.move.pepperos.io/ws"
-#define kPepperAPIURL @"https://dev.api.pepperos.io/authentication/byEmail";
-//#define kPepperAPIURL @"https://staging.api.pepperos.io/authentication/byEmail";
+//#define kPepperAPIURL @"https://dev.api.pepperos.io/authentication/byEmail";
+#define kPepperAPIURL @"https://staging.api.pepperos.io/authentication/byEmail";
 //#define kPepperAPIURL @"https://api.pepperos.io/authentication/byEmail";
 
 #define kMoveURLdev @"wss://172.16.30.66:3443/ws"
